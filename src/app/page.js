b04fd3eb7a3d678a4d7e168f22f6b43bf31975ef
Yogesh Patel <ygurjar932@gmail.com>
2025-05-21 18:47:44 +0530

    build

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import ServiceCard from "./Components/ServiceCard";
 import ServicesPage from "./Components/template";
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <ServicesPage/>
